3b61f7fe26f85c83035060a1055d12dd5a0be2f9
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-05-06 10:27:01 -0300

    Criando ProdutoController.

---
+++ src/main/java/com/mfs/apiestoqueprodutos/controller/ProdutoController.java
@@ -0,0 +1,45 @@
+package com.mfs.apiestoqueprodutos.controller;
+
+import com.mfs.apiestoqueprodutos.document.Produto;
+import com.mfs.apiestoqueprodutos.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class ProdutoController {
+
+    @Autowired
+    ProdutoService service;
+
+    @GetMapping(value = "/Produto")
+    public Flux<Produto> getProdutoAll(){
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/Produto/{id}")
+    public Mono<Produto> getProdutoId(String id){
+        return service.findByID(id);
+    }
+
+    @PostMapping(value = "/Produto")
+    public Mono<Produto> save(Produto produto){
+        return service.save(produto);
+    }
+
+    @PatchMapping(value = "/Produto")
+    public Mono<Produto> update(Produto produto){
+        return service.update(produto);
+    }
+
+    @DeleteMapping(value = "/Produto/{id}")
+    public Mono<Produto> delete(String id){
+        return service.delete(id);
+    }
+
+
+
+
+}
