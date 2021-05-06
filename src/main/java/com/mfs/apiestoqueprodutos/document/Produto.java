4764dd1ac4ac0347d8afa7bfaead78cbcb832c49
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-05-06 09:51:12 -0300

    Criando classe Produto.

---
+++ src/main/java/com/mfs/apiestoqueprodutos/document/Produto.java
@@ -0,0 +1,55 @@
+package com.mfs.apiestoqueprodutos.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Produto {
+
+    @Id
+    private String id ;
+    private String nome;
+    private Integer quantidade;
+    private Double preco;
+    private String descricao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
