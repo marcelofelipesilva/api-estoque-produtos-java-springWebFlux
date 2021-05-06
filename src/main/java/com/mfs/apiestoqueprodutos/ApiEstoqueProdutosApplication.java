87c6bf04365bec5599678c6ca51acb763eaf21d7
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-05-06 09:35:57 -0300

    Commit inicial sistema spring init

---
+++ src/main/java/com/mfs/apiestoqueprodutos/ApiEstoqueProdutosApplication.java
@@ -0,0 +1,15 @@
+package com.mfs.apiestoqueprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstoqueProdutosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiEstoqueProdutosApplication.class, args);
+		System.out.println("Sistema pronto rodando");
+	}
+
+}
---
+++ src/test/java/com/mfs/apiestoqueprodutos/ApiEstoqueProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfs.apiestoqueprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstoqueProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
